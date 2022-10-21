4ff434b7b06b6472cafb715769ee793297779c2e
Konstantinos <kostasmc2@gmail.com>
2022-10-21 15:17:08 +0300

    small fix

--- src/components/routes/DataVisualizationRoute.vue
+++ src/components/routes/DataVisualizationRoute.vue
@@ -1,29 +1,46 @@
 <template>
-    <div>
-        <div class="header-text">Visualizing Women’s Economic Rights Around the World</div>
-        <div class="text-center">
-            <div class="simple-text my-4">
-                Στο παρακάτω διάγραμμα φαίνεται σε απεικόνιση, η κατανομή των κοινωνικών δικαιωμάτων των γυναίκων σε όλο
-                τον κόσμο. Ενα πολυ ενδιαφερων γράφημα τόσο για τον τρόπο που παρουσιάζεται, αλλά και για το θέμα με το
-                οποίο ασχολήται. Μπορείτε με την χρήση της ροδέλας ή του PinchZoom να να κάνετε Zoom στον χάρτη για παρατήρηση
-                περαιτέρω πληροφοριών.
-            </div>
-            <v-zoomer style="width: 75%; height: auto; border: solid 1px silver; margin: 0 auto;">
-                <img
-                    src="@/assets/images/data-visual.jpg"
-                    style="object-fit: contain; width: 100%; height: 100%;"
-                >
-            </v-zoomer>
-            <div class="simple-text my-4">
-                Μπορείτε να δείτε ολόκληρη την έρευνα στο παρακάτω link : <a href="https://www.visualcapitalist.com/visualizing-womens-economic-rights-around-the-world/">Visualizing Women’s Economic Rights Around the World</a>
-            </div>
+    <div class="hello">
+        <div v-if="errorStr">
+            Sorry, but the following error
+            occurred: {{ errorStr }}
+        </div>
+    
+        <div v-if="gettingLocation">
+            <i>Getting your location...</i>
+        </div>
+    
+        <div v-if="location">
+            Your location data is {{ location.coords.latitude }}, {{ location.coords.longitude }}
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "DataVisualization"
+        name: "DataVisualization",
+        data(){
+            return{
+                location:null,
+                gettingLocation: false,
+                errorStr:null
+            }
+        },
+        created(){
+            if(!("geolocation" in navigator)) {
+                this.errorStr = 'Geolocation is not available.'
+                return
+            }
+
+            this.gettingLocation = true
+            // get position
+            navigator.geolocation.getCurrentPosition(pos => {
+                this.gettingLocation = false
+                this.location = pos
+            }, err => {
+                this.gettingLocation = false
+                this.errorStr = err.message
+            })
+        }
     }
 </script>
 
